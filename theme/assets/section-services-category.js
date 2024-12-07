c1184253d3483e3429641543a83f3b46e39137e5
Mayur2034 <mayur6563@gmail.com>
2024-12-07 19:16:47 +0530

    Design

---
+++ theme/assets/section-services-category.js
@@ -0,0 +1,26 @@
+class servicesCategory {
+  constructor(parent) {
+    if(!parent) {
+      return;
+    }
+
+    this.parent = parent;
+    this.init();
+  }
+
+  slider = () => {
+    let swiper = new Swiper('.services-category-media-js', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    })
+  }
+
+  init = () => {
+    this.slider();
+  }
+}
+new servicesCategory(document.querySelector('.services-category'));
