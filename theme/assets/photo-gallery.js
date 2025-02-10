a31c2b5f3667b3cd577b6fce0754e54790fcabac
sgurleendc <sgurleendc@gmail.com>
2025-02-11 00:53:38 +0530

    change

--- theme/assets/photo-gallery.js
+++ theme/assets/photo-gallery.js
@@ -1,62 +1,33 @@
 class PhotoGallery {
   constructor() {
+    this.PAGE_SIZE = 12;
+    this.currentPage = 1;
+    this.totalRecords = 0;
+    this.data = [];
     this.init();
   }
 
+  init() {
+    this.menuTabSwitch();
+    this.fetchData();
+    this.setupViewMoreButton();
+  }
+
   menuTabSwitch() {
-    // Select collection and menu switch elements
-    const collectionDivs = document.querySelectorAll(
-      ".photo-gallery .photo-gallery-wrapper .photo-gallery-container .gallery-wrappers"
-    );
     const menuSwitchDivs = document.querySelectorAll(
       ".photo-gallery .galleryname .name"
     );
-
+    
     if (menuSwitchDivs.length > 0) {
       menuSwitchDivs.forEach((ele) => {
         ele.addEventListener("click", () => {
-          const viewBTn = document.querySelector(".photo-gallery-button .view-more");
-          viewBTn.dataset.page = "1";
-
-          const collIDMenu = ele.getAttribute("data-handle");
-
+          this.PAGE_SIZE = 12;
+          this.currentPage = 1;
+          this.totalRecords = 0;
+          this.data = [];
+          this.fetchData(ele.dataset.name)
           menuSwitchDivs.forEach((item) => item.classList.remove("active"));
           ele.classList.add("active");
-
-          let galleryContainers =  document.querySelectorAll('.gallery-photo')
-          let max = parseInt(viewBTn.dataset.max);
-          let page = parseInt(viewBTn.dataset.page);
-          if(collIDMenu == ""){
-            galleryContainers.forEach((item,i)=>{
-                if(i+1 <= (max * page)){
-                  item.classList.remove('hidden')
-                }else{
-                  item.classList.add('hidden')
-                }
-                if((max * page) > galleryContainers.length){
-                  viewBTn.classList.add('hidden')
-                }
-                viewBTn.dataset.page = page
-            })
-            return;
-          }
-          galleryContainers.forEach((item) => item.classList.add('hidden'))
-          const filteredContainers = Array.from(galleryContainers).filter(item => item.dataset.name == collIDMenu);
-          filteredContainers.forEach((item, i) => {
-            if (i + 1 <= max) {
-              item.classList.remove('hidden');
-            } else {
-              item.classList.add('hidden');
-            }
-          });
-          
-          // Check if the view button should be hidden
-          if (filteredContainers.length > max) {
-            viewBTn.classList.remove('hidden');
-          }else{
-            viewBTn.classList.add('hidden');
-          }
-
         });
       });
     } else {
@@ -65,87 +36,80 @@ class PhotoGallery {
   }
 
   multiSlider() {
-    const sliderContainers = document.querySelectorAll(
+    document.querySelectorAll(
       ".photo-gallery-wrapper .photo-gallery-container .gallery-wrappers"
-    );
-    if (sliderContainers.length > 0) {
-      sliderContainers.forEach((ele) => {
-        const sliderWrapper = ele.querySelector(".myGallerySwiper");
-        new Swiper(sliderWrapper, {
-          slidesPerView: 1.2,
-          spaceBetween: 13,
-          centeredSlides: true,
-          initialSlide: 1,
-          loop: true,
-          breakpoints: {
-            768: {
-              slidesPerView: 2,
-            },
-          },
-        });
+    ).forEach((ele) => {
+      new Swiper(ele.querySelector(".myGallerySwiper"), {
+        slidesPerView: 1.2,
+        spaceBetween: 13,
+        centeredSlides: true,
+        initialSlide: 1,
+        loop: true,
+        breakpoints: {
+          768: { slidesPerView: 2 },
+        },
       });
-    }
-  }
-  randomHeight() {
-    const galleryPhotos = document.querySelectorAll(".gallery-photo");
-
-    // Loop through each image and set alternating heights
-    galleryPhotos.forEach((photo, index) => {
-      const image = photo.querySelector("img");
-      if (index % 2 === 0) {
-        photo.style.height = "300px"; // Set taller height for even index (0, 2, 4, ...)
-        image.style.height = "300px";
-      } else {
-        photo.style.height = "200px"; // Set shorter height for odd index (1, 3, 5, ...)
-        image.style.height = "200px";
-      }
     });
   }
-  viewMore() {
-    const viewBTn = document.querySelector(".photo-gallery-button .view-more");
-    const lessBtn = document.querySelector(".photo-gallery-button .view-less");
-    let  active = document.querySelector('.galleryname .name.active')
-    let galleryContainers = document.querySelectorAll(
-      ".photo-gallery .my-Gallery .gallery-photo"
-    );
-    if(active.dataset.handle != ""){
-      galleryContainers = document.querySelectorAll(
-        `.photo-gallery .my-Gallery .gallery-photo[data-name="${active.dataset.handle}"]`
-      );
-    }
-    if (viewBTn) {
-      viewBTn.addEventListener("click", () => {
-        let max = parseInt(viewBTn.dataset.max);
-        let page = parseInt(viewBTn.dataset.page) + 1;
-        galleryContainers.forEach((ele,i) => {
-          if(i+1 <= (max * page)){
-            ele.classList.remove("hidden");  
-          }
-          if((max * page) > galleryContainers.length){
-            viewBTn.classList.add('hidden')
-          }
-          viewBTn.dataset.page = page
-        });
-        
-      });
-    }
-    if (lessBtn) {
-      lessBtn.addEventListener("click", () => {
-        galleryContainers.forEach((ele, index) => {
-          if (index >= 7) {
-            ele.classList.add("hide");
-          }
-        });
-        lessBtn.classList.add("d-none");
-        viewBTn.classList.remove("d-none");
-      });
+
+  randomHeight() {
+    document.querySelectorAll(".gallery-photo").forEach((photo, index) => {
+      const image = photo.querySelector("img");
+      const height = index % 2 === 0 ? "300px" : "200px";
+      photo.style.height = height;
+      image.style.height = height;
+    });
+  }
+
+  async fetchData(category = "", page = 1) {
+    const url = `https://script.google.com/macros/s/AKfycbzYFAkKk4kez0rLzkXnNeUIh0vQhracBlqmlpdbmBzN2sorZQWQr2p7wwgSwz5Nh6Q8/exec?category=${category}&page=${page}&pageSize=${this.PAGE_SIZE}`;
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      
+      if (page === 1) this.data = result.data;
+      else this.data.push(...result.data);
+      
+      this.totalRecords = result.total;
+      this.displayPage();
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
   }
-  init() {
-    this.menuTabSwitch();
-    this.viewMore();
-    //this.randomHeight();
-    //this.multiSlider();
+
+  displayPage() {
+    const galleryContainer = document.querySelector(".gallery-slider");
+    galleryContainer.innerHTML = "";
+    
+    this.data.forEach(({ Category, Image, Title }) => {
+      const div = document.createElement("div");
+      div.className = "gallery-photo";
+      div.setAttribute("data-name", Category);
+      div.innerHTML = `
+        <div class="image-with-text__media-item">
+          <div class="image-with-text__media">
+            <img src="${Image}" alt="${Title}" width="1500" height="1939">
+          </div>
+          <div class="photo-gallery-content">
+            <div class="photo-gallery-text">
+              <p>${Title}</p>
+            </div>
+          </div>
+        </div>`;
+      galleryContainer.appendChild(div);
+    });
+
+    document.querySelector(".photo-gallery-button").classList.toggle(
+      "hidden",
+      this.data.length >= this.totalRecords
+    );
+  }
+
+  setupViewMoreButton() {
+    document.querySelector(".view-more").addEventListener("click", () => {
+      this.currentPage++;
+      this.fetchData("", this.currentPage);
+    });
   }
 }
 
